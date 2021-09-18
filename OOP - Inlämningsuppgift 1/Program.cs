ea1a0b5958ff236fc72c8f3adab125181be8398e
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2021-09-18 15:10:10 +0200

    fixade while(true) satsen

--- OOP - Inlämningsuppgift 1/Program.cs
+++ OOP - Inlämningsuppgift 1/Program.cs
@@ -11,88 +11,137 @@ namespace OOP___Inlämningsuppgift_1
             int satsning = 0;
             int lyckoTal = 0;
             bool spelarSpelet = true;
-            bool negativ = satsning < 0;
+            int tärning;
 
             Random rnd = new Random();
-            int tärningA = rnd.Next(0, 7);
-            int tärningB = rnd.Next(0, 7);
-            int tärningC = rnd.Next(0, 7);
+            tärning = rnd.Next(0, 7);
 
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Välkommen till att spela Fortuna!");
+            Console.WriteLine("---------------------------------");
+            
             Console.WriteLine($"Du har {saldo} pix");
 
-            while (spelarSpelet)
+            while (saldo > 50)
             {
-                Console.Write("Hur mycket vågar du satsa? ");
-                string inputSats = Console.ReadLine();
-                int.TryParse(inputSats, out satsning);
-
-                if (negativ)
+                while (true)
                 {
-                    Console.WriteLine("Du angav ett felaktigt värde, försök igen!");
-                }
-                else
-                {
-                    continue;
-                }
-                
-                
-                else if (lyckoTal == 0)
-                {
-                    Console.Write("Ange ditt lyckotal (1-6): ");
-                    string inputLyckotal = Console.ReadLine();
+                    Console.Write("Hur mycket vågar du satsa? ");
+                    string inputSats = Console.ReadLine();
+                    bool satsningOk = int.TryParse(inputSats, out satsning);
 
-                    bool lyckotalOk = int.TryParse(inputLyckotal, out lyckoTal);
-
+                    if(!satsningOk)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Hoppsan.. Ange endast siffror!");
+                        continue;
+                    }
+                    if (satsning<50)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Kom igen! Lite mer vågar du väl satsa? ;)");
+                    }
+                    else if (satsning>saldo)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Du har för lite pix! Satsa mindre");
+                    }
                 }
-            
+
+
+                //for (int i = 0; i < 3; i++)
+                //{
+
+                //}
+
             }
 
-            bool rättA = satsning == tärningA;
-            bool rättB = satsning == tärningB;
-            bool rättC = satsning == tärningC;
 
-            if (rättA || rättB || rättC)
-            {
-                if (rättA && rättB && rättC)
-                {
-                    Console.WriteLine();
-                }
-                if (lyckoTal < 1 || lyckoTal > 6)
-                {
-                    Console.WriteLine("Hoppsan! Du angav ett felaktigt värde");
-                }
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Tärning A: {tärningA} ");
-                    Console.WriteLine($"Tärning B: {tärningB} ");
-                    Console.WriteLine($"Tärning C: {tärningC} ");
-                }
 
+
+
+            //        if ()
+            //        {
+            //            Console.Write("Hur mycket vågar du satsa? ");
+            //            string inputSats = Console.ReadLine();
+            //            bool satsningOk = int.TryParse(inputSats, out satsning);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Du angav ett felaktigt värde, försök igen!");
+            //        }
+
+
+                    //        if (lyckoTal == 0)
+                    //        {
+                    //            Console.Write("Ange ditt lyckotal (1-6): ");
+                    //            string inputLyckotal = Console.ReadLine();
+
+                    //            bool lyckotalOk = int.TryParse(inputLyckotal, out lyckoTal);
+
+                    //        }
+
+                    //    }
+
+                    //    bool rättA = satsning == tärningA;
+                    //    bool rättB = satsning == tärningB;
+                    //    bool rättC = satsning == tärningC;
+
+                    //    if (rättA || rättB || rättC)
+                    //    {
+                    //        if (rättA && rättB && rättC)
+                    //        {
+                    //            satsning *= 4;
+                    //            Console.WriteLine(satsning);
+                    //        }
+
+                    //    }    
+
+                    //    if (lyckoTal < 1 || lyckoTal > 6)
+                    //        {
+                    //            Console.WriteLine("Hoppsan! Du angav ett felaktigt värde");
+                    //        }
+                    //        else
+                    //        {
+                    //            Console.WriteLine();
+                    //            Console.WriteLine($"Tärning A: {tärningA} ");
+                    //            Console.WriteLine($"Tärning B: {tärningB} ");
+                    //            Console.WriteLine($"Tärning C: {tärningC} ");
+                    //        }
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.WriteLine("");
+
+                    //    switch ()
+
+                    //    //case 1: alla tre rätt
+
+                    //    //case 2: två rätt
+
+                    //    //case 3: en rätt
+
+                    //    //case 4: inga rätt
+                    //    case 
+
+
+                    //    //att spela igen
+
+                    //        //bearbeta data
+
+
+
+
+                    //        //if (saldo < 50) break;
+
+
+
+                    //        //Skriv ut resultatet
+                    //    }
+                    //}
 
 
             }
-            else
-            {
-                Console.WriteLine("");
-
-
-
-
-
-
-                //bearbeta data
-
-
-
-
-                //if (saldo < 50) break;
-
-
-
-                //Skriv ut resultatet
-            }
-        }
     }
 }
 
